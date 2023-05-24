99949d768631cca4afdb0b8f63a096c89d032ed8
Gabriel Nunes <gnunesmail@gmail.com>
2023-05-24 15:10:43 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,24 +20,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <p>Organização de eventos, com foco em experiências espetaculares.</p>
-        <div className={styles.events}>
-        <a
-          href="https://shiftfestival.cc"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/shift.svg"
-            alt="Sh*ft Festival"
-            width={100}
-            height={50}
-          />
-        </a>
-        <a href="https://codecon.dev" target="_blank" rel="noopener noreferrer">
-          <Image src="/codecon.svg" alt="Codecon" width={180} height={60} />
-        </a>
-        </div>
+        <p>Consultoria e organização de eventos.</p>
       </footer>
     </div>
   );
