902eb9b1a307fa12ce30e99acac0865e198fac7e
Gabriel Nunes <gnunesmail@gmail.com>
2026-02-12 21:10:28 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/', // The incoming request path pattern
+        destination: 'https://codecon.dev/', // The path you want to route to
+        permanent: true, // true for 308, false for 307
+      },
+      // You can add more redirects here
+    ];
+  },
 }
